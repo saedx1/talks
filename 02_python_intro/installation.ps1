011f413d192064a82a7845f3548ed785db4a1593
saedx1 <saadmtsa@gmail.com>
2021-10-15 22:38:47 -0400

    adding everything

---
+++ 02_python_intro/installation.ps1
@@ -0,0 +1,9 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install miniconda3 -y
+
+conda create -n saed python=3.8
+
+conda activate saed
+
+conda install jupyter
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# talks
+# Talks
+
+This repository holds all information given in my talks.
+
+1. First Steps in a Software Engineering Career - GDSC Palestine Polytechnic
+2. Introduction to Python: a language for everyone! - GDSC Palestine Polytechnic
