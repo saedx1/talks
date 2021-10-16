ddfca8682986d73e524882020af3871eed0f74da
saedx1 <saadmtsa@gmail.com>
2021-10-16 11:02:44 -0400

    update slides and content

--- 02_python_intro/installation.ps1
+++ 02_python_intro/installation.ps1
@@ -1,9 +1,13 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install miniconda3 -y
+choco install miniconda3 --params="'/AddToPath:1'" -y
 
-conda create -n saed python=3.8
+conda create -n saad python=3.7
 
-conda activate saed
+conda activate saad
 
-conda install jupyter
+conda install ipykernel jupyter
+
+python -m ipykernel install --user --name saad
+
+jupyter notebook
---
+++ 02_python_intro/starting.ps1
@@ -0,0 +1,9 @@
+cd Desktop # changes the folder you are inside
+
+mkdir learning_python # creates a new folder
+
+cd learning_python
+
+conda activate saad # activates your enivornment
+
+jupyter notebook # starts your in-browser editor
